393d2044976e0abd49d6139547cb6d3c321bb2ed
Cristina Lugo <cmlugoce@syr.edu>
2018-06-17 13:50:15 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
     end 
     
     def execute_transaction
-      if self.valid? && @status == "pending"
+      if @sender.balance . @amount && @status == "pending"
         @sender.balance -= @amount 
         @receiver.balance += @amount 
         self.status = "complete"
