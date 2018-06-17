063265ed203fd18901d8feb64eb05a058b23293c
Cristina Lugo <cmlugoce@syr.edu>
2018-06-17 13:29:42 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,8 +2,10 @@ class Transfer
   
   attr_accessor 
   
-  def initialize(sender)
+  def initialize(sender, receiver, status)
     @sender = sender
+    @receiver = receiver
+    @status = "pending"
     
   end
 end
