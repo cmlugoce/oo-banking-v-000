f28fc7678176c57e7663c11098a199d20675ed89
Cristina Lugo <cmlugoce@syr.edu>
2018-06-17 13:49:02 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,6 +23,8 @@ class Transfer
      
       else 
         
+        @status = "rejected"
+        return "Transaction rejected. Please check your account balance."
       end 
     end 
         
